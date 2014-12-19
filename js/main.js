8b9bc9961f2f3a123b169a8324eda5e2a677d37e
Jan Smolders <jansmolders86@gmail.com>
2014-12-19 16:35:53 +0100

    New site design - Inital commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function($){
+  "use strict";
+	window.scrollTo(0,0);
+
+	$(".scroll-link").click(function(e) {
+		e.preventDefault();
+		var scrollLink = $(this).attr('href')
+		 $('html, body').animate({
+			 scrollTop: $(scrollLink).offset().top - 50
+		 }, 1000);
+	});
+	
+})(jQuery);
